7a52e15e0772c04cd4204b4245391a27f17e4f56
Pavankumar K R <pavan123456pop@gmail.com>
2025-05-21 10:45:01 +0530

    add (app) folder

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button className="text-lg">Button</Button>
+      <p>hello world</p>
+    </div>
+  );
+}
