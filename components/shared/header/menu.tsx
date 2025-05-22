bf1afad9449bae6abfda28e8172fa347b8f71b89
Pavankumar K R <pavan123456pop@gmail.com>
2025-05-22 20:22:25 +0530

    add menu component

---
+++ components/shared/header/menu.tsx
@@ -0,0 +1,54 @@
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { EllipsisVertical, ShoppingCart, User } from "lucide-react";
+import Link from "next/link";
+import ModeToggle from "./mode-toggle";
+
+export default function Menu() {
+  return (
+    <div className="flex justify-end gap-3">
+      <nav className="hidden md:flex w-full max-w-xs gap-1">
+        <ModeToggle />
+        <Button asChild variant="ghost">
+          <Link href="/cart">
+            <ShoppingCart /> Cart
+          </Link>
+        </Button>
+        <Button asChild variant="default">
+          <Link href="/sign-in">
+            <User /> Sign in
+          </Link>
+        </Button>
+      </nav>
+      <nav className="md:hidden">
+        <Sheet>
+          <SheetTrigger className="align-middle">
+            <EllipsisVertical />
+          </SheetTrigger>
+          <SheetContent className="flex flex-col items-start">
+            <SheetHeader>
+              <SheetTitle>Menu</SheetTitle>
+              <ModeToggle />
+              <Button asChild variant="ghost">
+                <Link href="/cart">
+                  <ShoppingCart /> Cart
+                </Link>
+              </Button>
+              <Button asChild variant="default">
+                <Link href="/sign-in">
+                  <User /> Sign in
+                </Link>
+              </Button>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </nav>
+    </div>
+  );
+}
