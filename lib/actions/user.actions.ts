b55959e1f0c6a94813dd5111f01b189925b2e729
Pavankumar K R <pavan123456pop@gmail.com>
2025-06-02 17:12:32 +0530

    add user action files

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,16 @@
+import { signIn } from "@/auth";
+import { signInFormSchema } from "../validators";
+
+export async function signInWithCredentials(
+  prevState: unknown,
+  formData: FormData,
+) {
+  try {
+    const user = signInFormSchema.parse({
+      email: formData.get("email"),
+      password: formData.get("password"),
+    });
+    await signIn("credentials", user);
+    return { success: true, message: "Signed in successfully" };
+  } catch (error) {}
+}
