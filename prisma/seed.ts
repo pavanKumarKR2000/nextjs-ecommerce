10567b6b44f7497ad0cbc6eb74554f545baef988
Pavankumar K R <pavan123456pop@gmail.com>
2025-06-02 14:50:24 +0530

    minor change

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,6 +2,12 @@ import sampleData from "@/db/sample-data";
 import prisma from "@/lib/prisma";
 async function main() {
   await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
+
+  await prisma.user.createMany({ data: sampleData.users });
   await prisma.product.createMany({ data: sampleData.products });
   console.log("Database seeded successfully!");
 }
