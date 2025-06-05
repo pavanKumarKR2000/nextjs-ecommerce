054d924007979c50bd40fa7bd3c33c5009c94063
Pavankumar K R <pavan123456pop@gmail.com>
2025-06-05 19:52:12 +0530

    minor change

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -8,9 +8,14 @@ export const LATEST_PRODUCTS_LIMIT =
   Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
 
 export const signInDefaultValues = {
-  email: "admin@example.com",
-  password: "123456",
+  email: "",
+  password: "",
 };
+
+// export const signInDefaultValues = {
+//   email: "admin@example.com",
+//   password: "123456",
+// };
 
 export const signUpDefaultValues = {
   name: "",
